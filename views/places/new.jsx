32b2117c76e1b1598bae158e9f344695c25a0ff7
Maria Rios <kanelapr@gmail.com>
2022-07-25 15:38:45 -0400

    form data connected

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -20,11 +20,11 @@ function new_form () {
                         <input className="form-control" id="city" name="city" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="state">Place Name</label>
+                        <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="cuisines">Place Name</label>
+                        <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required/>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
