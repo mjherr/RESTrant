6ede0a6f7e8d955dcd2195584dd6c5583649cefb
Maria Rios <kanelapr@gmail.com>
2022-07-25 11:47:14 -0400

    Adding places controller

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//router
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+//wildcard route
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
