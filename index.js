342275d6838ce045e3f8cf45e8baf49cd81ba103
Maria Rios <kanelapr@gmail.com>
2022-07-25 14:16:36 -0400

    Adding styles

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 
 //router
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 app.use('/places', require('./controllers/places'))
 
