7dc8b57a2e2f725839724f30873d6dfdbe399ac2
mjherr <kanelapr@gmail.com>
2022-08-12 22:04:05 -0500

    part 11 part 1

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {averageRating} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -33,6 +47,10 @@ function show (data) {
                     </div>
                 </div>
                 <h1>{data.place.name}</h1>
+                <h2>
+                    Rating
+                </h2>
+                {rating}
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
